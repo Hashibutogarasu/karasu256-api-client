8dce44e5620950c09ab5d23ccf83f447d3166d5f
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-12 22:40:37 +0900

    バージョンを3.3.21に更新し、OpenAPIドキュメントのバージョンを反映。新しいAPIエンドポイントとモデルを追加。

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -4,6 +4,7 @@ export * from './ArtifactSetsApi';
 export * from './ArtifactsApi';
 export * from './CharactersApi';
 export * from './GalleriesApi';
+export * from './Hi3CharactersApi';
 export * from './RegionsApi';
 export * from './VersionsApi';
 export * from './WeaponsApi';
